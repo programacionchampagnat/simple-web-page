ab623bb241c4607414de05610cb1c1e0a68a0066
Sebastian Vera <sebastian.vera@colegiochampagnat.com.ar>
2019-03-26 15:22:02 -0300

    first commit

---
+++ js/index.js
@@ -0,0 +1,17 @@
+var sum = function(a, b) {
+  return a + b;
+}
+
+var substraction = function(a, b) {
+  return a - b;
+}
+
+window.onload = function() {
+  // We will not use this yet
+
+  // var result = [];
+  // result.push(sum(2,3), substraction(3,1));
+  // document.getElementsByClassName('result')[0].innerText = result;
+
+  console.log("Hello World!");
+};
